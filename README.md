781f7ec65788a0309e25229cf4c5885d382bf61c
Felipe Silva <felipe.dacs@gmail.com>
2020-03-11 23:45:54 -0300

    feat: add estrutura inicial react

---
+++ README.md
@@ -0,0 +1,5 @@
+# react-chat
+
+## Development server
+- ``npm run dev`` dentro do projeto levanta o servidor em `http://localhost:8080`
+
---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    presets: [
+        "@babel/preset-env", // transpila ES
+        "@babel/preset-react" // transpila particularidades do reach, como o jsx
+    ]
+}
